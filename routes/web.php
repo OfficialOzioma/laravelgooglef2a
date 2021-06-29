076ea6ed5f3bb615718a715dcc501d0264f33cf1
Ozioma Dike <pascalozioma18@gmail.com>
2021-06-29 12:09:47 +0100

    Laravel Google 2FA

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/complete-registration', 'Auth\RegisterController@completeRegistration');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/2fa', function () {
+    return redirect(URL()->previous());
+})->name('2fa')->middleware('2fa');
+
+Route::get('/profile','HomeController@profile')->name('profile');
+Route::get('/about','HomeController@about')->name('about');
+
+Route::get('/re-authenticate', 'HomeController@reauthenticate');
